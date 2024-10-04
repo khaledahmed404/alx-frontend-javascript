36e30eea5886ec31523d698807fe7a8f4f18861f
khaled <khaledahmed102000@gmail.com>
2024-10-04 07:34:02 -0700

    f

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,6 @@
+bob@dylan:~$ cat 2-main.js
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
